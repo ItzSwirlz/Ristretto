a63947a9db62cda3267bc76a4b2cb6afb68b5820
Joshua Peisach <itzswirlz2020@outlook.com>
2024-08-17 13:35:25 -0400

    Support current title requests, clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <http/http.hpp>
 #include <malloc.h>
 #include <nn/ac.h>
+#include <nn/acp/title.h>
 #include <nn/act.h>
 #include <sysapp/launch.h>
 #include <wups.h>
@@ -32,7 +33,7 @@ WUPS_PLUGIN_LICENSE("BSD");
 
 **/
 
-WUPS_USE_WUT_DEVOPTAB();           // Use the wut devoptabs
+WUPS_USE_WUT_DEVOPTAB();       // Use the wut devoptabs
 WUPS_USE_STORAGE("ristretto"); // Unique id for the storage api
 
 HttpServer server;
@@ -40,90 +41,105 @@ bool server_made = false;
 static std::vector<std::string> messages;
 
 #define ENABLE_SERVER_DEFAULT_VALUE true
-#define ENABLE_SERVER_CONFIG_ID "enableServer"
+#define ENABLE_SERVER_CONFIG_ID     "enableServer"
 
 bool enableServer = ENABLE_SERVER_DEFAULT_VALUE;
 
 void make_server() {
     if (server_made) {
-		return;
+        return;
     }
-	
-    server_made = true;	
-	DEBUG_FUNCTION_LINE("Server started.");
-	
-	try {
 
-		// Empty endpoint to allow for device discovery.
-		server.when("/")->requested([](const HttpRequest &req) {
-			return HttpResponse{200};
-		});
+    server_made = true;
+    DEBUG_FUNCTION_LINE("Server started.");
 
-		// Shuts down the console regardless of what state it currently is in.
-		server.when("/shutdown")->posted([](const HttpRequest &req) {
-			OSShutdown();
-			
-			return HttpResponse{200};
-		});
+    try {
 
-		// Reboot the console regardless of what state it currently is in.
-		server.when("/reboot")->posted([](const HttpRequest &req) {
-			OSForceFullRelaunch();
-			SYSLaunchMenu();
+        // Empty endpoint to allow for device discovery.
+        server.when("/")->requested([](const HttpRequest &req) {
+            return HttpResponse{200};
+        });
 
-			return HttpResponse{200};
-		});
+        // Shuts down the console regardless of what state it currently is in.
+        server.when("/shutdown")->posted([](const HttpRequest &req) {
+            OSShutdown();
 
-		// Gets the device serial number.
-		server.when("/serial")->requested([](const HttpRequest &req) {
-			// Credit to .danielko on Discord
-			int handle = MCP_Open();
-			if (handle < 0) { // some error?
-				throw std::runtime_error{"MCP_Open() failed with error " + std::to_string(handle)};
-			}
-			
-			MCPSysProdSettings settings alignas(0x40);
-			MCPError error = MCP_GetSysProdSettings(handle, &settings);
-			MCP_Close(handle);
-			if (error) {
-				DEBUG_FUNCTION_LINE_ERR("Error at MCP_GetSysProdSettings");
-				return HttpResponse{500, "text/plain", "Couldn't get the serial!"}; 
-			}
-			
-			DEBUG_FUNCTION_LINE_INFO("Obtained serial: %s", settings.serial_id);
-			return HttpResponse{200, "text/plain", settings.serial_id};
-		});
+            return HttpResponse{200};
+        });
 
-		// Launches the Wii U Menu
-		server.when("/launch/menu")->posted([](const HttpRequest &req) {
-			// FIXME: May lock up when the plugin is inactive, like in friends list
-			SYSLaunchMenu();
-			return HttpResponse{200};
-		});
+        // Reboot the console regardless of what state it currently is in.
+        server.when("/reboot")->posted([](const HttpRequest &req) {
+            OSForceFullRelaunch();
+            SYSLaunchMenu();
 
-		// Launches the current title's manual
-		server.when("/launch/emanual")->posted([](const HttpRequest &req) {
-			// FIXME: If the title has no manual, DO NOT SWITCH!!!! IT WILL LOCKUP THE SYSTEM! (eg Friends List)
-			SYSSwitchToEManual();
-			return HttpResponse{200};
-		});
+            return HttpResponse{200};
+        });
 
-		// TODO: Make the port configurable
-		server.startListening(8572);
-	} 
-	catch (std::exception &e) {
-		DEBUG_FUNCTION_LINE_INFO("got error: %s\n", e.what());
-	}
+        // Gets the device serial number.
+        server.when("/serial")->requested([](const HttpRequest &req) {
+            // Credit to .danielko on Discord
+            int handle = MCP_Open();
+            if (handle < 0) { // some error?
+                throw std::runtime_error{"MCP_Open() failed with error " + std::to_string(handle)};
+            }
+
+            MCPSysProdSettings settings alignas(0x40);
+            MCPError error = MCP_GetSysProdSettings(handle, &settings);
+            MCP_Close(handle);
+            if (error) {
+                DEBUG_FUNCTION_LINE_ERR("Error at MCP_GetSysProdSettings");
+                return HttpResponse{500, "text/plain", "Couldn't get the serial!"};
+            }
+
+            DEBUG_FUNCTION_LINE_INFO("Obtained serial: %s", settings.serial_id);
+            return HttpResponse{200, "text/plain", settings.serial_id};
+        });
+
+        server.when("/currenttitle")->requested([](const HttpRequest &req) {
+            ACPTitleId id;
+            ACPResult res = ACPGetTitleIdOfMainApplication(&id);
+            if (res) {
+                DEBUG_FUNCTION_LINE_ERR("Error at ACPGetTitleIdOfMainApplication");
+                return HttpResponse{500, "text/plain", "Couldn't get the title!"};
+            }
+            ACPMetaXml *meta = new ACPMetaXml;
+            res              = ACPGetTitleMetaXml(id, meta);
+            if (res) {
+                DEBUG_FUNCTION_LINE_ERR("Error at ACPGetTitleMetaXml");
+                return HttpResponse{500, "text/plain", "Couldn't get the title!"};
+            }
+            return HttpResponse{200, "text/plain", meta->shortname_en};
+        });
+
+        // Launches the Wii U Menu
+        server.when("/launch/menu")->posted([](const HttpRequest &req) {
+            // FIXME: May lock up when the plugin is inactive, like in friends list
+            SYSLaunchMenu();
+            return HttpResponse{200};
+        });
+
+        // Launches the current title's manual
+        server.when("/launch/emanual")->posted([](const HttpRequest &req) {
+            // FIXME: If the title has no manual, DO NOT SWITCH!!!! IT WILL LOCKUP THE SYSTEM! (eg Friends List)
+            SYSSwitchToEManual();
+            return HttpResponse{200};
+        });
+
+        // TODO: Make the port configurable
+        server.startListening(8572);
+    } catch (std::exception &e) {
+        DEBUG_FUNCTION_LINE_INFO("got error: %s\n", e.what());
+    }
 }
 
 void stop_server() {
     server.shutdown();
     server_made = false;
-	DEBUG_FUNCTION_LINE("Server shut down.");
+    DEBUG_FUNCTION_LINE("Server shut down.");
 }
 
 void make_server_on_thread() {
-	try {
+    try {
         std::jthread thready(make_server);
         thready.detach();
     } catch (std::exception &e) {
@@ -132,11 +148,11 @@ void make_server_on_thread() {
 }
 
 void enableServerChanged(ConfigItemBoolean *item, bool newValue) {
-	// If the value has changed, we store it in the storage.
-	if (newValue != enableServer)
-		WUPSStorageAPI::Store(ENABLE_SERVER_CONFIG_ID, newValue);
-    
-	enableServer = newValue;
+    // If the value has changed, we store it in the storage.
+    if (newValue != enableServer)
+        WUPSStorageAPI::Store(ENABLE_SERVER_CONFIG_ID, newValue);
+
+    enableServer = newValue;
 }
 
 WUPSConfigAPICallbackStatus ConfigMenuOpenedCallback(WUPSConfigCategoryHandle rootHandle) {
@@ -157,19 +173,19 @@ WUPSConfigAPICallbackStatus ConfigMenuOpenedCallback(WUPSConfigCategoryHandle ro
 
 void ConfigMenuClosedCallback() {
     WUPSStorageAPI::SaveStorage();
-	
-	if (server_made && !enableServer) stop_server();
-	else if (!server_made && enableServer) {
-		make_server_on_thread();
-	}
+
+    if (server_made && !enableServer) stop_server();
+    else if (!server_made && enableServer) {
+        make_server_on_thread();
+    }
 }
 
 // Gets called ONCE when the plugin was loaded.
 INITIALIZE_PLUGIN() {
     // Logging only works when compiled with `make DEBUG=1`. See the README for more information.
     WHBLogCafeInit();
-	WHBLogUdpInit();
-	
+    WHBLogUdpInit();
+
     DEBUG_FUNCTION_LINE("Hello world! - Ristretto");
 
     WUPSConfigAPIOptionsV1 configOptions = {.name = "Ristretto"};
@@ -184,14 +200,14 @@ INITIALIZE_PLUGIN() {
     if ((storageRes = WUPSStorageAPI::SaveStorage()) != WUPS_STORAGE_ERROR_SUCCESS) {
         DEBUG_FUNCTION_LINE_ERR("SaveStorage failed: %s (%d)", WUPSStorageAPI_GetStatusStr(storageRes), storageRes);
     }
-	
-	if (!enableServer) return;
-	make_server_on_thread();
+
+    if (!enableServer) return;
+    make_server_on_thread();
 }
 
 // Gets called when the plugin will be unloaded.
 DEINITIALIZE_PLUGIN() {
     WHBLogUdpDeinit();
-	WHBLogCafeDeinit();
+    WHBLogCafeDeinit();
     DEBUG_FUNCTION_LINE("Ristretto deinitializing.");
 }
